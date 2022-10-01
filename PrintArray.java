f9a69ec492ca75c4c6799797d802654cebdc6598
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-10-01 16:35:26 +0530

    Print the Elements of an Array

---
+++ PrintArray.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		String name[]= {"Vikas", "Sanket", "Ritik", "Nitin", "Kunal"};
+		for(int i=0;i<name.length;i++) {
+			System.out.println(name[i]);
+		}
+	}
+}
